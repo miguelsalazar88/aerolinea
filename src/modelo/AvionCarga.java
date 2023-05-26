d96c69b8f374967f2c03c3614e76c2768ebe20b0
Miguel Salazar <miguelsalazar88@gmail.com>
2023-05-26 12:13:34 -0500

    create SistemaSingleton

--- src/modelo/AvionCarga.java
+++ src/modelo/AvionCarga.java
@@ -3,7 +3,7 @@ package modelo;
 public class AvionCarga extends Avion {
     private int capacidad;
 
-    public AvionCarga(int id, String marca, String tipo, int capacidad, int carga) {
+    public AvionCarga(int id, String marca, String tipo, int capacidad) {
         super(id, marca, tipo);
         this.capacidad = capacidad;
     }
---
+++ src/modelo/SistemaSingleton.java
@@ -0,0 +1,32 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class SistemaSingleton implements Serializable {
+
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Aerolinea> aerolineas;
+    private static SistemaSingleton instancia;
+
+    private SistemaSingleton(){
+        this.usuarios = new ArrayList<Usuario>();
+        this.aerolineas = new ArrayList<Aerolinea>();
+    }
+
+    public static SistemaSingleton getInstance(){
+        if (instancia == null){
+            instancia = new SistemaSingleton();
+        }
+        return instancia;
+    }
+
+    public void registrarUsuario(Usuario u){
+        this.usuarios.add(u);
+    }
+
+    public void registrarAerolinea(Aerolinea a){
+        this.aerolineas.add(a);
+    }
+
+}
