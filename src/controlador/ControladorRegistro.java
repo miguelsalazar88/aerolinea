8a845289920679c2b3c64037a32659cb4afcf866
Miguel Salazar <miguelsalazar88@gmail.com>
2023-05-31 22:37:41 -0500

    verificacion de campos en ControladorRegistro

--- src/controlador/ControladorRegistro.java
+++ src/controlador/ControladorRegistro.java
@@ -5,6 +5,7 @@ import modelo.Usuario;
 import modelo.UsuarioFactory;
 import vista.VentanaRegistro;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class ControladorRegistro extends Controlador{
@@ -19,6 +20,7 @@ public class ControladorRegistro extends Controlador{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (e.getSource().equals(this.vista.getRegistrarBtn())){
 
             //Se toman los campos de la ventana para asignar a las variables
@@ -33,14 +35,27 @@ public class ControladorRegistro extends Controlador{
                 salario = Integer.parseInt(vista.getSalarioField().getText());
             }
 
+            if(tipoUsuario.isEmpty() || username.isEmpty() || password.isEmpty()||
+                    nombre.isEmpty() || apellido.isEmpty()){
+                JOptionPane.showMessageDialog(vista,"Por favor, complete todos los campos.",
+                        "Campos incompletos", JOptionPane.ERROR_MESSAGE);
 
-            //Se crea el usuario con el Factory Method y se registra en el sistema
-            Usuario usuario = UsuarioFactory.crearUsuario(tipoUsuario, username, password, nombre, apellido,cargo, salario);
-            sistema.registarUsuario(usuario);
-            for (Usuario u:
-                 sistema.getUsuarios()) {
-                System.out.println(u.getNombre());
             }
+
+            else if(SistemaSingleton.getInstance().usuarioExiste(username)){
+                JOptionPane.showMessageDialog(
+                        vista,
+                        "Por favor utilice otro numbre de usuario.",
+                        "Usuario Existente",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
+            else{
+                //Se crea el usuario con el Factory Method y se registra en el sistema
+                Usuario usuario = UsuarioFactory.crearUsuario(tipoUsuario, username, password, nombre, apellido,cargo, salario);
+                sistema.registarUsuario(usuario);
+            }
+
         }
     }
 }
